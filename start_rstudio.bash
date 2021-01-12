18fb0d30cb1605f8cbae94565d80c0076affe3d5
Maxime RIO <maxime.rio@niwa.co.nz>
2021-01-12 00:36:54 +0000

    swap fixed ports

--- start_rstudio.bash
+++ start_rstudio.bash
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 if [[ $# -ne 1 ]]; then
-    echo "Usage: $(basename $0) RSTUDIO_PORT"
+    echo "Usage: $(basename $0) NGINX_PORT"
     exit 1
 fi
 
-RSTUDIO_PORT="$1"
+NGINX_PORT="$1"
 
-NGINX_PORT="21200"  # TODO autodetect a free port
+RSTUDIO_PORT="21200"  # TODO autodetect a free port
 IMAGE="/home/riom/nobackup/tidyverse_nginx_3.6.1.sif"  # TODO use image in /opt/nesi/containers
 
 module load Singularity
