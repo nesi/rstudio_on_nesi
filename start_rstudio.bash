4ed6dc285e9ced520ccb7f4741ff64918e6791a5
Maxime RIO <maxime.rio@niwa.co.nz>
2021-01-12 01:09:39 +0000

    add option to set base url

--- start_rstudio.bash
+++ start_rstudio.bash
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ $# -ne 1 ]]; then
-    echo "Usage: $(basename $0) NGINX_PORT"
+if [[ $# -lt 1 ]]; then
+    echo "Usage: $(basename $0) NGINX_PORT [BASE_URL]"
     exit 1
 fi
 
 NGINX_PORT="$1"
+BASE_URL="${2:-user-redirect/proxy/$NGINX_PORT}"
 
 RSTUDIO_PORT="21200"  # TODO autodetect a free port
 IMAGE="/home/riom/nobackup/tidyverse_nginx_3.6.1.sif"  # TODO use image in /opt/nesi/containers
@@ -16,4 +17,4 @@ export PASSWORD="blabla"
 
 singularity run --contain \
     -B "$HOME","/nesi/project/$SLURM_JOB_ACCOUNT","/nesi/nobackup/$SLURM_JOB_ACCOUNT" \
-    "$IMAGE" "$RSTUDIO_PORT" "$NGINX_PORT"
+    "$IMAGE" "$RSTUDIO_PORT" "$NGINX_PORT" "$BASE_URL"
