6ec1314d6d21c8d03875592c5eafc503aadc93e9
Maxime RIO <maxime.rio@niwa.co.nz>
2021-01-20 04:34:22 +0000

    move RSTUDIO port allocation logic inside the container

--- start_rstudio.bash
+++ start_rstudio.bash
@@ -8,9 +8,8 @@ fi
 NGINX_PORT="$1"
 BASE_URL="${2:-user-redirect/proxy/$NGINX_PORT}"
 
-# trick to find a free port (see https://unix.stackexchange.com/a/132524 and jupyter-server-proxy source code)
-RSTUDIO_PORT="$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')"
-IMAGE="/home/riom/nobackup/tidyverse_nginx_3.6.1.sif"  # TODO use image in /opt/nesi/containers
+# TODO use image in /opt/nesi/containers
+IMAGE="/home/riom/nobackup/tidyverse_nginx_3.6.1.sif"
 
 module load Singularity
 
@@ -18,4 +17,4 @@ export PASSWORD="blabla"
 
 singularity run --contain \
     -B "$HOME","/nesi/project/$SLURM_JOB_ACCOUNT","/nesi/nobackup/$SLURM_JOB_ACCOUNT" \
-    "$IMAGE" "$RSTUDIO_PORT" "$NGINX_PORT" "$BASE_URL"
+    "$IMAGE" "$NGINX_PORT" "$BASE_URL"
