16d3344f69e4af8e9726a0d98c622a4895839828
Maxime RIO <maxime.rio@niwa.co.nz>
2021-01-21 02:40:01 +0000

    make the shell script as minimal as possible (will be removed eventually)

--- start_rstudio.bash
+++ start_rstudio.bash
@@ -1,20 +1,9 @@
 #!/usr/bin/env bash
 
-if [[ $# -ne 2 ]]; then
-    echo "Usage: $(basename $0) NGINX_PORT PROXY_URL"
-    exit 1
-fi
-
-NGINX_PORT="$1"
-PROXY_URL="$2"
-
-# TODO use image in /opt/nesi/containers
-IMAGE="/home/riom/nobackup/tidyverse_nginx_3.6.1.sif"
+set -u
 
 module load Singularity
 
-export PASSWORD="blabla"
-
-singularity run --contain \
+PASSWORD="blabla" singularity run --contain \
     -B "$HOME","/nesi/project/$SLURM_JOB_ACCOUNT","/nesi/nobackup/$SLURM_JOB_ACCOUNT" \
-    "$IMAGE" "$NGINX_PORT" "$PROXY_URL"
+    "/home/riom/nobackup/tidyverse_nginx_3.6.1.sif" "$1" "$2"
