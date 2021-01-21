161521c43691df83d7f1915199c9b6ef81d34570
Maxime RIO <maxime.rio@niwa.co.nz>
2021-01-21 02:33:38 +0000

    minor changes

--- start_rstudio.bash
+++ start_rstudio.bash
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ $# -lt 1 ]]; then
-    echo "Usage: $(basename $0) NGINX_PORT [BASE_URL]"
+if [[ $# -ne 2 ]]; then
+    echo "Usage: $(basename $0) NGINX_PORT PROXY_URL"
     exit 1
 fi
 
 NGINX_PORT="$1"
-BASE_URL="${2:-user-redirect/proxy/$NGINX_PORT}"
+PROXY_URL="$2"
 
 # TODO use image in /opt/nesi/containers
 IMAGE="/home/riom/nobackup/tidyverse_nginx_3.6.1.sif"
@@ -17,4 +17,4 @@ export PASSWORD="blabla"
 
 singularity run --contain \
     -B "$HOME","/nesi/project/$SLURM_JOB_ACCOUNT","/nesi/nobackup/$SLURM_JOB_ACCOUNT" \
-    "$IMAGE" "$NGINX_PORT" "$BASE_URL"
+    "$IMAGE" "$NGINX_PORT" "$PROXY_URL"
