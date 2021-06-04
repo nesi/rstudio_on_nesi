f6ee1ce432c7815fd23c5c9a558994f19849451a
Maxime RIO <maxime.rio@niwa.co.nz>
2021-06-04 01:37:32 +0000

    add openssh-client to the container

--- rstudio_on_nesi/__init__.py
+++ rstudio_on_nesi/__init__.py
@@ -34,7 +34,7 @@ def setup_rstudio():
             "--writable-tmpfs",
             "-B",
             f'"{home_path}","/nesi/project/{account}","/nesi/nobackup/{account}"',
-            "/opt/nesi/containers/rstudio-server/tidyverse_nginx_4.0.1__v0.9.sif",
+            "/opt/nesi/containers/rstudio-server/tidyverse_nginx_4.0.1__v0.10.sif",
             "{port}",
             "{base_url}/proxy/{port}",
         ],
