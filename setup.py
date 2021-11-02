ee2c067e9c4374a0b1e25525cef40921caddaee8
Callum Walley <callum.walley@nesi.org.nz>
2021-11-02 13:02:08 +1300

    Change name and iterate version

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="jupyter-rstudio-on-nesi",
-    version="0.10.0",
+    name="jupyter-rstudio-on-nesi_dev",
+    version="0.20.0",
     packages=find_packages(),
-    package_data={"rstudio_on_nesi": ["rstudio_logo.svg"]},
+    package_data={"rstudio_on_nesi_dev": ["rstudio_logo.svg"]},
     entry_points={
-        "jupyter_serverproxy_servers": ["rstudio = rstudio_on_nesi:setup_rstudio"]
+        "jupyter_serverproxy_servers": ["rstudio_dev = rstudio_on_nesi_dev:setup_rstudio"]
     },
     install_requires=["jupyter-server-proxy"],
 )
