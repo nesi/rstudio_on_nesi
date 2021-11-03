0c3907768cf7545e5b599beecd740efbeff75e75
Callum Walley <callum.walley@nesi.org.nz>
2021-11-04 10:21:46 +1300

    Moved shell scripts to package directory

--- rstudio_on_nesi_dev/__init__.py
+++ rstudio_on_nesi_dev/__init__.py
@@ -24,16 +24,14 @@ def setup_rstudio():
     except FileNotFoundError:
         rstudio_password = None
 
-    icon_path = pkg_resources.resource_filename("rstudio_on_nesi", "rstudio_logo.svg")
+    icon_path = pkg_resources.resource_filename("rstudio_on_nesi_dev", "rstudio_logo.svg")
+    wrapper_path = pkg_resources.resource_filename("rstudio_on_nesi_dev", "singularity_wrapper.sh")
+    runscript_path = pkg_resources.resource_filename("rstudio_on_nesi_dev", "singularity_runscript.sh")
 
     return {
         "command": [
-            "/home/cwal219/project/dev_rstudio_on_nesi_centos/conf/singularity_wrapper.sh",
-            "run",
-            "--contain",
-            "--writable-tmpfs",
-            "/home/cwal219/project/dev_rstudio_on_nesi_centos/conf/nesi_base.sif",
-            #"/opt/nesi/containers/rstudio-server/tidyverse_nginx_4.0.1__v0.10.sif",
+            wrapper_path,
+            runscript_path,
             "{port}",
             "{base_url}/proxy/{port}",
         ],
