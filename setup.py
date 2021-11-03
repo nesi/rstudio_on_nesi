cea67e271b93980261ded28efd9409f7569f505c
Callum Walley <callum.walley@nesi.org.nz>
2021-11-04 10:15:08 +1300

    Rename launcher icon

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 
 setup(
-    name="jupyter-rstudio-on-nesi_dev",
+    name="jupyter-rstudio-on-nesi-dev",
     version="0.20.1",
     packages=find_packages(),
-    package_data={"rstudio_on_nesi": ["rstudio_logo.svg", "singularity_wrapper.sh", "singularity_runscript.sh"]},
+    package_data={"rstudio_on_nesi_dev": ["rstudio_logo.svg", "singularity_wrapper.sh", "singularity_runscript.sh"]},
     entry_points={
         "jupyter_serverproxy_servers": ["rstudio_dev = rstudio_on_nesi_dev:setup_rstudio"]
     },
