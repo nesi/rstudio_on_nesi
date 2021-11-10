c328342320ed15e1fc79e3e275f813d426c00fbc
Maxime RIO <maxime.rio@niwa.co.nz>
2021-11-10 00:27:37 +0000

    remove dead code in setup.py + reformat

--- setup.py
+++ setup.py
@@ -5,12 +5,17 @@ setup(
     name="jupyter-rstudio-on-nesi-dev",
     version="0.21.0",
     packages=find_packages(),
-    package_data={"rstudio_on_nesi_dev": ["rstudio_logo.svg", "singularity_wrapper.sh", "singularity_runscript.sh"]},
+    package_data={
+        "rstudio_on_nesi_dev": [
+            "rstudio_logo.svg",
+            "singularity_wrapper.sh",
+            "singularity_runscript.sh",
+        ]
+    },
     entry_points={
-        "jupyter_serverproxy_servers": ["rstudio_dev = rstudio_on_nesi_dev:setup_rstudio"]
+        "jupyter_serverproxy_servers": [
+            "rstudio_dev = rstudio_on_nesi_dev:setup_rstudio"
+        ]
     },
     install_requires=["jupyter-server-proxy"],
-    # cmdclass={
-    #     'install': InstallCommand,
-    # }
 )
