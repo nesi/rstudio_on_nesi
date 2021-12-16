3ff236fbce011b5f6316032442fb35e2d2b9682a
Callum M Walley <35017184+CallumWalley@users.noreply.github.com>
2021-12-16 15:03:59 +1300

    Appeasement of Maximes high formatting standards.

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ setup(
     version="0.21.0",
     packages=find_packages(),
     package_data={
-        "rstudio_on_nesi_dev": [
+        "rstudio_on_nesi_dev":[
             "rstudio_logo.svg",
             "singularity_wrapper.sh",
             "singularity_runscript.sh",
         ]
     },
     entry_points={
-        "jupyter_serverproxy_servers": [
-            "rstudio_dev = rstudio_on_nesi_dev:setup_rstudio"
+        "jupyter_serverproxy_servers":[
+            "rstudio_dev=rstudio_on_nesi_dev:setup_rstudio"
         ]
     },
     install_requires=["jupyter-server-proxy"],
