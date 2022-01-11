a469fd5b76678cf608e98585cac334367779bb81
Maxime RIO <maxime.rio@niwa.co.nz>
2022-01-11 00:37:51 +0000

    simplify setup_rstudio function, no need for function to retrieve sif_path

--- rstudio_on_nesi_dev/__init__.py
+++ rstudio_on_nesi_dev/__init__.py
@@ -1,16 +1,15 @@
 import os
 import subprocess
-import pkg_resources
 import secrets
+from pkg_resources import resource_filename
 from pathlib import Path
 
 
-# TODO Document this. v
-# If .config/rstudio_on_nesi/singularity_image_path exists, the path specified in here will be used over default image.
+# default image if none is configured in .config/rstudio_on_nesi/singularity_image_path
+DEFAULT_SIF_PATH = "/opt/nesi/containers/rstudio-server/rstudio_server_on_centos7.sif"
+
 
 def setup_rstudio():
-    default_sif_path="/opt/nesi/containers/rstudio-server/rstudio_server_on_centos7.sif"
-
     try:
         rstudio_config_folder = Path(os.environ["XDG_CONFIG_HOME"]) / "rstudio_on_nesi"
     except KeyError:
@@ -23,29 +22,17 @@ def setup_rstudio():
 
     rstudio_password = password_file.read_text().rstrip()
 
-    # Get absoulte path of resources.
     pkg_path = "rstudio_on_nesi_dev"
-    icon_path = pkg_resources.resource_filename(
-        pkg_path, "rstudio_logo.svg"
-    )
-    wrapper_path = pkg_resources.resource_filename(
-        pkg_path, "singularity_wrapper.bash"
-    )
-    runscript_path = pkg_resources.resource_filename(
-        pkg_path, "singularity_runscript.bash"
-    )
+    icon_path = resource_filename(pkg_path, "rstudio_logo.svg")
+    wrapper_path = resource_filename(pkg_path, "singularity_wrapper.bash")
+    runscript_path = resource_filename(pkg_path, "singularity_runscript.bash")
 
-    # May want to add aditional logic to make more failproof.
-    def get_sif_path():
-        
-        try:
-            with open(rstudio_config_folder / "singularity_image_path", "r") as f:
-                result =  f.readline()
-        except:
-            result =  default_sif_path
-
-        print(f"Using image at '{result}'")
-        return result
+    sif_path_conf = rstudio_config_folder / "singularity_image_path"
+    try:
+        sif_path = sif_path_conf.read_text().rstrip()
+    except FileNotFoundError:
+        sif_path = DEFAULT_SIF_PATH
+    print(f"Using image at '{sif_path}'")
 
     return {
         "command": [
@@ -55,7 +42,7 @@ def setup_rstudio():
             "{base_url}rstudio_dev",
         ],
         "timeout": 15,
-        "environment": {"PASSWORD": rstudio_password, "SIFPATH":get_sif_path()},
+        "environment": {"PASSWORD": rstudio_password, "SIFPATH": sif_path},
         "absolute_url": False,
         "launcher_entry": {
             "icon_path": icon_path,
