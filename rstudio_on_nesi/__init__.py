3aea3515b0f7cead52a4513e3f7d8c11645345d0
mattbixley <mattbixley72@gmail.com>
2023-06-20 14:17:30 +1200

    version 0.25.0

--- rstudio_on_nesi/__init__.py
+++ rstudio_on_nesi/__init__.py
@@ -5,7 +5,7 @@ from pkg_resources import resource_filename
 from pathlib import Path
 
 
-__version__ = "0.24.0"
+__version__ = "0.25.0"
 
 # default image if none configured in ~/.config/rstudio_on_nesi/singularity_image_path
 DEFAULT_SIF_PATH = f"/opt/nesi/containers/rstudio-server/rstudio_server_on_centos7__v{__version__}.sif"
